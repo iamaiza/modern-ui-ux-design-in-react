08896fac478424a1511d153f3e1c7e5199324180
iamaiza <aiza9382@gmail.com>
2022-12-27 20:35:43 +0500

    added blog

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({ imgUrl, date, title }) => {
   return (
-    <div>
-        Article
+    <div className='gpt3__blog-container_article d-flex'>
+      <div className='gpt3__blog-container_article-img'>
+        <img src={imgUrl} alt='blogImg'  />
+      </div>
+      <div className='gpt3__blog-container_article-content d-flex'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
     </div>
   )
 }
