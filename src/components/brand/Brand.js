63295ca18b62bb5c935168337a88a8e5efbeb2cc
iamaiza <aiza9382@gmail.com>
2022-12-28 13:31:15 +0500

    added class

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -5,20 +5,20 @@ import { google, slack, atlassian, dropbox, shopify } from './imports'
 
 const Brand = () => {
   return (
-    <div className='gpt3__brand section__padding d-flex align-center'>
-      <div className='d-flex align-center'>
+    <div className='gpt3__brand section__padding d-flex align-center justify-center'>
+      <div className='d-flex align-center justify-center'>
         <img src={google} alt= 'google' />
       </div>
-      <div className='d-flex align-center'>
+      <div className='d-flex align-center justify-center'>
         <img src={slack} alt= 'slack' />
       </div>
-      <div className='d-flex align-center'>
+      <div className='d-flex align-center justify-center'>
         <img src={atlassian} alt= 'atlassian' />
       </div>
-      <div className='d-flex align-center'>
+      <div className='d-flex align-center justify-center'>
         <img src={dropbox} alt= 'dropbox' />
       </div>
-      <div className='d-flex align-center'>
+      <div className='d-flex align-center justify-center'>
         <img src={shopify} alt= 'shopify' />
       </div>
     </div>
